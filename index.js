0e1ddf6c93c0d08cdb2bf8ff5b09579e04c9a200
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2022-01-05 00:56:15 -0700

    can now add new comments

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+let idCount = 0
+
 // creates new object for each comment
 class Comment {
-    constructor(user, createdAt, content, score, replies, currentUser, commentId) {
+    constructor(commentId, user, createdAt, content, score, replies) {
         this.user = user
         this.createdAt = createdAt
         this.content = content
         this.score = score
         this.replies = replies
-        this.currentUser = currentUser
         this.commentId = commentId
     }
     createComment() {
         const div = document.createElement("div")
         div.classList.add("comment-container")
+        let isCurrentUser = this.user.username === currentUser.username ? true : false
         const divContent = `
             <div class="comment">
                 <div class="comment-info">
                     <img src="${this.user.image.png}" class="profile-picture">
                     <p class="username">${this.user.username}</p>
-                    <p class="${this.currentUser ? 'current-user-indicator' : ''}">${this.currentUser ? "you" : ""}</p>
+                    <p class="${isCurrentUser ? 'current-user-indicator' : ''}">${isCurrentUser ? "you" : ""}</p>
                     <p class="created-at">${this.createdAt}</p>
                 </div>
                 <p class="comment-content">${this.content}</p>
@@ -26,7 +28,7 @@ class Comment {
                     <p id="score" data-comment-id=${this.commentId}>${this.score}</p>
                     <img class="minus score-change" data-comment-id=${this.commentId} src="./images/icon-minus.svg">
                 </div>
-                <div class="reply">
+                <div class="reply" data-comment-id=${this.commentId}>
                     <img src="./images/icon-reply.svg">
                     <p>Reply</p>
                 </div>
@@ -38,21 +40,36 @@ class Comment {
 
         this.element = div
     }
-    createReply(user, createdAt, content, score, currentUser, commentId) {
+    // adds or subtracts score and updates html
+    updateScore(add) {
+        if (add) this.score++
+        else this.score--
+        document.querySelector(`[data-comment-id='${this.commentId}']`).textContent = this.score
+    }
+}
+
+// reply to be added to comments
+class Reply extends Comment {
+    constructor(commentId, user, createdAt, content, score, replyTo) {
+        super(commentId, user, createdAt, content, score)
+        this.replyTo = replyTo
+    }
+    createReply() {
         const div = document.createElement("div")
         div.classList.add("reply-box")
+        let isCurrentUser = this.user.username === currentUser.username ? true : false
         const divContent = `
             <div class="comment-info">
-                <img src="${user.image.png}" class="profile-picture">
-                <p class="username">${user.username}</p>
-                <p class="${currentUser ? 'current-user-indicator' : ''}">${currentUser ? "you" : ""}</p>
-                <p class="created-at">${createdAt}</p>
+                <img src="${this.user.image.png}" class="profile-picture">
+                <p class="username">${this.user.username}</p>
+                <p class="${isCurrentUser ? 'current-user-indicator' : ''}">${isCurrentUser ? "you" : ""}</p>
+                <p class="created-at">${this.createdAt}</p>
             </div>
-            <p class="comment-content">${content}</p>
+            <p class="comment-content">${this.content}</p>
             <div class="comment-score">
-                <img class="plus score-change" data-comment-id=${commentId} src="./images/icon-plus.svg">
-                <p id="score" data-comment-id=${commentId}>${score}</p>
-                <img class="minus score-change" data-comment-id=${commentId} src="./images/icon-minus.svg">
+                <img class="plus score-change" data-comment-id=${this.commentId} src="./images/icon-plus.svg">
+                <p id="score" data-comment-id=${this.commentId}>${this.score}</p>
+                <img class="minus score-change" data-comment-id=${this.commentId} src="./images/icon-minus.svg">
             </div>
             <div class="reply">
                 <img src="./images/icon-reply.svg">
@@ -61,13 +78,7 @@ class Comment {
         `
         div.innerHTML = divContent
 
-        this.element.append(div)
-    }
-    updateScore(add) {
-        console.log(document.querySelector(`[data-comment-id='${this.commentId}']`))
-        if (add) this.score++
-        else this.score--
-        document.querySelector(`[data-comment-id='${this.commentId}']`).textContent = this.score
+        this.replyTo.append(div)
     }
 }
 
@@ -80,9 +91,8 @@ async function getData() {
 
     currentUser = data.currentUser
     data.comments.forEach(comment => {
-        const { user, createdAt, content, score, replies, id } = comment
-        console.log(user)
-        const commentObj = new Comment(user, createdAt, content, score, replies, user.username === currentUser.username ? true : false, id)
+        const { id, user, createdAt, content, score, replies } = comment
+        const commentObj = new Comment(id, user, createdAt, content, score, replies)
         comments.push(commentObj)
     })
     
@@ -90,17 +100,26 @@ async function getData() {
     loadComments()
 }
 
+// loads all comments to DOM
 function loadComments() {
     // clears comments
     document.querySelector(".comments").innerHTML = ""
 
     // create comments
     comments.forEach( comment => {
+        idCount += 1
         // get data from json file
         comment.createComment()
+        let replies = []
         comment.replies.forEach( reply => {
-            comment.createReply(reply.user, reply.createdAt, reply.content, reply.score, reply.user.username === currentUser.username ? true : false, reply.id)
+            const newReply = new Reply(reply.id, reply.user, reply.createdAt, reply.content, reply.score, comment.element)
+            replies.push(newReply)
+            idCount += 1
         })
+        replies.forEach( reply => {
+            reply.createReply()
+        })
+        comment.replies = replies
     })
 
     document.querySelector("#create-comment-img").setAttribute("src", currentUser.image.png)
@@ -129,3 +148,14 @@ function changeScore({currentTarget}) {
     })
     loadComments()
 }
+
+// create a new comment
+function createComment() {
+    const newCommentText = document.querySelector("#new-comment-text").value
+    if (newCommentText !== "") {
+        const newComment = new Comment(idCount, currentUser, "just now", newCommentText, 0, [])
+        comments.push(newComment)
+        newComment.createComment()
+    }
+}
+document.querySelector('#create-comment').addEventListener("click", createComment)
